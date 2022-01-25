3657338eff388ba088369d48148daa6e4b23a839
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2022-01-25 23:33:10 +0900

    Move migrations dir

--- db/migrate.sh
+++ db/migrate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-source .env
+source ../.env
 
 MYSQL_URL="mysql://landin:landin@tcp(127.0.0.1:3306)/landin"
 echo "docker run -v $PWD/migrations:/migrations --network host migrate/migrate -database ${MYSQL_URL} -path $PWD/database/migrations $1 $2"
