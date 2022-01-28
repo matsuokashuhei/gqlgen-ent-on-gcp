cc0d65e273d826a570c61ef7f3c55a57871bed6c
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2022-01-29 01:21:17 +0900

    Set up Gin

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,32 +1,35 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-gonic/gin"
 	"github.com/matsuokashuhei/landin/graph/generated"
 	"github.com/matsuokashuhei/landin/graph/resolver"
 	"github.com/matsuokashuhei/landin/pkg/database"
+	"gorm.io/gorm"
 )
 
 const defaultPort = "8080"
 
+func graphqlHandler(db *gorm.DB) gin.HandlerFunc {
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{DB: db}}))
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
 func main() {
 	db, _ := database.Connect()
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{DB: db}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	r := gin.Default()
+	r.POST("/query", graphqlHandler(db))
+	r.GET("/", playgroundHandler())
+	r.Run()
 }
