60dc6f6aae19ded5cd2b8ba26e7abb0b60d388ff
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2022-05-02 14:30:48 +0900

    Small changes (#10)
    * Rename
    
    * Remove `db/migrations`
    
    * Rename `docker-compose.yml`

--- db/migrations/000002_create_schools_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS schools (
-    `id` bigint(20) AUTO_INCREMENT NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(`id`)
-);
