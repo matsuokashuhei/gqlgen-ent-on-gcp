1ee7bb207cd368b22de330d837a2a98c2b4806d4
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2022-05-05 02:44:55 +0900

    :construction:

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,11 @@
+import { Menu, Notifications } from "@mui/icons-material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 import { VFC } from "react";
 
 export const Header: VFC = () => {
   return (
-    <div className="flex h-16 items-center">
-      <div className="flex flex-shrink-0">LOGO</div>
-    </div>
+    <AppBar position="static">
+      <Toolbar variant="regular"></Toolbar>
+    </AppBar>
   );
 };
--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,14 +1,16 @@
+import { Group, Home, Person, School } from "@mui/icons-material";
 import {
-  CalendarIcon,
-  HomeIcon,
-  UserIcon,
-  UsersIcon,
-} from "@heroicons/react/solid";
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import { VFC } from "react";
-import { Link } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
 
 export const SideBar: VFC = () => {
+  const navigate = useNavigate();
+  /*
   const { signOut } = useAuth();
 
   const renderSignOutButton = () => <button onClick={signOut}>Sign Out</button>;
@@ -39,5 +41,42 @@ export const SideBar: VFC = () => {
       </div>
       <div className="absolute bottom-0 left-0">{renderSignOutButton()}</div>
     </div>
+  );
+  */
+  return (
+    <List component="nav">
+      <Link to="/">
+        <ListItemButton>
+          <ListItemIcon>
+            <Home />
+          </ListItemIcon>
+          <ListItemText primary="HOME" />
+        </ListItemButton>
+      </Link>
+      <Link to="/classes">
+        <ListItemButton>
+          <ListItemIcon>
+            <School />
+          </ListItemIcon>
+          <ListItemText primary="CLASS" />
+        </ListItemButton>
+      </Link>
+      <Link to="/members">
+        <ListItemButton>
+          <ListItemIcon>
+            <Group />
+          </ListItemIcon>
+          <ListItemText primary="MEMBER" />
+        </ListItemButton>
+      </Link>
+      <Link to="/instructors">
+        <ListItemButton>
+          <ListItemIcon>
+            <Person />
+          </ListItemIcon>
+          <ListItemText primary="INSTRUCTOR" />
+        </ListItemButton>
+      </Link>
+    </List>
   );
 };
